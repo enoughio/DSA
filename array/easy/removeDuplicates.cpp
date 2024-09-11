bb0fcf4b2d45186d418593340daaf31eea784a49
Ani <turboji007@gmail.com>
2024-09-11 23:39:54 +0530

    solved some older problems

---
+++ array/easy/removeDuplicates.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int i = 0;
+        
+        for(int j = 1; j< nums.size(); j++)
+        {    if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+            return i+1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     int removeDuplicates(vector<int>& nums) {
+//         int i=0;
+//         for(int j=1;j<nums.size();j++)
+//         {
+//             if(nums[i] != nums[j])
+//             {
+//                 i++;
+//                 nums[i] = nums[j];
+//             }
+//         }
+//         return i+1;
+        
+//     }
+// };
