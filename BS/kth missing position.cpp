a578cc40039ba62b0f2b908221d5c32a4a6fae1e
Ani <turboji007@gmail.com>
2024-09-29 10:33:01 +0530

    better brute

--- BS/kth missing position.cpp
+++ BS/kth missing position.cpp
@@ -31,3 +31,48 @@ public:
         
     }
 };
+
+
+
+
+/// even better brute force
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+
+        vector<int>mis (1001, 0);
+        int n = 1, cnt = 0;
+        int s = arr.size();
+
+        for(int i = 0; i < s; i++){
+            if(arr[i] <= k) k++;
+            else break;
+        }
+
+        return k;
+
+        // int i = 0;
+        // while(i < s){
+        //     if(arr[i] == n){
+        //         n++;
+        //         i++;
+        //     } else {
+        //         cnt++;
+        //         mis[cnt] = n;
+        //         n++;
+        //     }
+        // }
+
+
+        // for(int i = cnt+1; i < 1001; i++){
+        //     mis[i] = n;
+        //     n++;
+        // }
+
+ 
+        // return mis[k];
+
+        
+    }
+};
