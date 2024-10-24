987998b5f72083e9d1b38f0a8d6b456f7aaef168
Ani <turboji007@gmail.com>
2024-10-24 22:07:33 +0530

    count no dist elem

---
+++ string/count substring with k distinct elem.cpp
@@ -0,0 +1,45 @@
+
+class Solution
+{
+  public:
+    long long int substrCount (string s, int k) {
+    	//code here.
+    	int n = s.length();
+    	
+    	int i = 0;
+    	int ans = 0;
+    	int gap = k;
+    	
+    	    
+    	    
+    
+    	while(gap < n){
+    	    
+    	  int ws = 0, we =gap; // create a window
+    	  while(we < n) {// moving window till end
+    	    
+    	        map<char, int>mapp;
+    	        int dist = 0;
+    	       
+    	        for( int j = ws; j <= we; j++){    // count number of distinct character
+    	          if (mapp.find(s[j]) == mapp.end()) {
+                        dist++;
+                        mapp[s[j]] = 1;
+                    }
+
+    	        }
+                    
+                if(dist == k){
+                    ans++;
+                }	        
+    	    // move window
+    	   ws++;
+    	   we++;
+        }
+        gap++;  // increase size of window
+    }
+    
+        return ans;
+    }
+    
+};
