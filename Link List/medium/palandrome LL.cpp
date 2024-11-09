e5727e7fc8915b0451108f1f1c2b8cdf361db924
Ani <turboji007@gmail.com>
2024-11-09 23:39:16 +0530

    done palandrome LL

--- Link List/medium/palandrome LL.cpp
+++ Link List/medium/palandrome LL.cpp
@@ -10,10 +10,86 @@
  */
 class Solution {
 public:
-
-    // basicly create a fale list at the begining wit the values of node then compare it;
-    ListNode* InsetPNode(ListNode * pHead, int data){
+    ListNode * reverse(ListNode * newHead){
+        ListNode* prev = nullptr;
+        ListNode * front;
         
+        while(newHead != nullptr){
+            front = newHead->next;
+            newHead->next = prev;
+            prev = newHead;
+            newHead = front;
+        }
+
+        return prev;
+    }
+
+
+    bool isPalindrome(ListNode* head) {
+
+        if(head == NULL || head->next == nullptr)
+            return true;
+
+        ListNode * slow = head;
+        ListNode * fast = head;
+
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+
+        // ListNode * MidHead = slow->next;
+        ListNode * MidHead = reverse(slow->next);
+        ListNode * second = MidHead;
+        ListNode * first = head;
+
+        while(second != nullptr){
+            if(first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        // reverse(slow->next);
+        return true;
+    }
+};
+
+/*
+    better
+        {
+
+        stack<int> st;
+        ListNode* it = head;
+
+        while (it != nullptr) {
+            int data = it->val;
+            st.push(data);
+            it = it->next;
+        }
+        it = head;
+        while (it != nullptr) {
+            int data = it->val;
+            
+            if(data != st.top())
+                return false;
+
+            st.pop();
+            it = it->next;
+        }
+
+        return true;
+
+    }
+
+
+
+    brute
+    // basicly create a fale list at the begining wit the values of node then
+   compare it; ListNode* InsetPNode(ListNode * pHead, int data){
+
         ListNode* newPHead = new ListNode(data);
         newPHead->next = pHead;
 
@@ -21,7 +97,7 @@ public:
     }
 
     bool isPalindrome(ListNode* head) {
-        
+
         int data;
         ListNode * it = head;
         ListNode * pHead = head;
@@ -32,14 +108,13 @@ public:
 
             it = it->next;
         }
- 
         it = head;
 
         while(it !=  nullptr){
-            
+
             if(pHead->val != it->val)
                 return false;
-            
+
             pHead = pHead->next;
             it = it->next;
 
@@ -49,4 +124,4 @@ public:
 
 
     }
-};
+*/
