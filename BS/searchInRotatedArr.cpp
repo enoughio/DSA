5c1b95a996f92a93be0a3240f00df117f12d76bc
Ani <turboji007@gmail.com>
2025-02-16 09:33:59 +0530

    solved search in rotated array using Python

--- BS/searchInRotatedArr.cpp
+++ BS/searchInRotatedArr.cpp
@@ -53,7 +53,31 @@ public:
 
 
 
+// in python
+class Solution:
+    def search(self, nums: List[int], t: int) -> int:
+        s = 0
+        e = len(nums)-1
 
+
+        while( s <= e):
+            mid = (s+e)//2
+            
+            if(nums[mid] == t):
+                return mid
+            
+            if nums[mid] <= nums[e] :
+                if t >= nums[mid] and t <= nums[e] :
+                    s = mid + 1
+                else:
+                    e = mid - 1
+            else : 
+                if t >= nums[s] and t <= nums[mid] :
+                    e = mid - 1
+                else :
+                    s = mid + 1
+        
+        return -1
 
 
 
