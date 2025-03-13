d42638bf52df4cc63dc18e09efe4f0fb1b6a9cbc
Ani <turboji007@gmail.com>
2025-03-13 10:00:47 +0530

    Add solution for valid anagram and include necessary headers in C++ file

--- string/ValidAnagram.py
+++ string/ValidAnagram.py
@@ -19,3 +19,28 @@ class Solution:
                 return False
 
         return True
+    
+
+
+#  print(m1[ ord(i) - ord('a') ])
+
+#  more optimized solution
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if(len(s) != len(t)):
+            return False
+
+        m1 = [0]*26
+        
+        for i in s :
+            m1[ ord(i) - ord('a') ] += 1
+           
+        for i in t:
+            if m1[ ord(i) - ord('a') ] > 0:
+                m1[ ord(i) - ord('a') ]-=1
+            else: 
+                return False
+
+        return True
