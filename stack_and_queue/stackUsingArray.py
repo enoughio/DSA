70d2ca374a4718b1b36fd9b4acc614d61e06f69b
Ani <turboji007@gmail.com>
2025-07-22 23:50:55 +0530

    solved more question on stack

--- stack_and_queue/stackUsingArray.py
+++ stack_and_queue/stackUsingArray.py
@@ -2,26 +2,34 @@
 # stack using array
 class StackImpl () :  
 
-    def __init__(self):
+    def __init__(self, capacity):
         self.top = -1
-        self.stack = []
+        self.capacity =  capacity
+        self.stack = [0] * capacity
 
 
     def topElem(self) :
         if self.top == -1 :
-            return -1
+            return None
         return self.stack[self.top]
     
     
-    def push(self, n) : 
+    def push(self, n) :
+        if( self.top >= self.capacity - 1 ):
+            print("Overflow") 
+            return None
+        
         self.top += 1
-        self.stack.append(n)
-        return self.stack[self.top]
+        self.stack[self.top] = n
+        return n
     
     
     def pop(self) :
-        if self.top == -1 :
+
+        if self.top <= -1 :
+            print("Underflow")
             return None
+        
         self.top -= 1
         return self.stack[self.top+1]
     
@@ -29,15 +37,24 @@ class StackImpl () :
     def size(self) :
         return self.top + 1
     
+    def is_full(self) : 
+        return self.top == self.capacity -1 
 
-s = StackImpl()
+    def is_empty(self) : 
+        return self.top == -1
+    
+
+s = StackImpl(5)
+
 s.push(10)
 s.push(20)
-print(s.topElem())  # 20
-print(s.pop())         # 20
-print(s.topElem())  # 20
-print(s.size())        # 1
+print(f'{s.topElem()}')    # 20
+s.push(30)
 
+print(s.pop())     # 30
+print(s.size())    # 2
 
-s2 = StackImpl()
-print(f'this is s2 {s2.topElem()}')
+s.push(40)
+s.push(50)
+s.push(60)
+s.push(70)         # Should print "Stack Overflow"
