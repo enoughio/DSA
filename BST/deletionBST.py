37357255d844916839f25fc0e96c02e887cfe93d
aniket <aniketjatav.dev@gmail.com>
2026-02-14 08:55:47 +0530

    still  solving deletion in BST

--- BST/deletionBST.py
+++ BST/deletionBST.py
@@ -66,6 +66,59 @@ class Solution:
             
 
 
+# ------------------------- day 2 solution -------------------------
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        # searching 
+        node = root 
+
+        while( node ) : 
+
+            if node.val > key : 
+                if node.left  and node.left.val == key : 
+                    node.left = self.delete(node.left)
+                    break
+                else  : 
+                    node = node.left 
+
+            else : 
+               
+                if node.right  and node.right.val == key : 
+                    node.right = self.delete(node.right)
+                    break
+                else : 
+                    node = node.right
+                
+        return root
+            
+    def delete(self, node ) : 
+        left = node.left
+        
+        if node.right : 
+            right = node.right
+            rm = self.fetchLeftMost(right)
+            node = TreeNode(rm.val)
+            
+            node.left = left
+            node.right = right
+        else : 
+            return left
+    
+    def fetchLeftMost(node) : 
+        
+        if node.left and node.left.left is None  : 
+            return node
+        
+        
+    
 
 
     
