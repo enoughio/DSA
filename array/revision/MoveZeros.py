65775cc5445dbd73ae8e769016bcb5afa96208ee
aniket <aniketjatav.dev@gmail.com>
2026-02-23 08:05:35 +0530

    solved move zeros

---
+++ array/revision/MoveZeros.py
@@ -0,0 +1,26 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0 
+        j = 0
+
+        n = len(nums)
+
+        while j < n : 
+            
+            if j < i : 
+                j+=1
+                continue 
+            
+            if nums[i] != 0 : 
+                i+=1 
+            
+            if nums[j] == 0 :
+                j+=1
+            
+            if j < n and i < n and j > i and nums[i] == 0 and nums[j] != 0 : 
+                nums[i], nums[j] = nums[j], nums[i]
+            
+        
