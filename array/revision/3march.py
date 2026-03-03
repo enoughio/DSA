209ea099821b1551723f1995e93b74e5f0de1d33
aniket <aniketjatav.dev@gmail.com>
2026-03-03 14:49:15 +0530

    done some easy thing

---
+++ array/revision/3march.py
@@ -0,0 +1,51 @@
+
+# find no of subarry with sum k 
+
+arr = [2,3,4,1,1,4,1,3,4,3,2]
+k = 6
+
+def noOfSubarray(arr, k) : 
+     
+    i = 0 
+    j = 0 
+    cnt = 0 
+
+    sum = 0
+
+    while (j < len(arr)) : 
+
+        if sum < 0 : 
+            sum = 0 
+
+        while sum > k :  
+            sum -= arr[i] 
+            i+=1 
+
+        sum += arr[j] 
+        j+=1 
+
+        if sum == k : 
+            cnt+=1 
+    
+    return cnt 
+
+-----------------------
+
+# ----------Two Sum--------------
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        mp = {}
+
+        for i in range(len(nums)) : 
+
+            rem = target - nums[i]
+
+            if rem in mp : 
+                return [i, mp[rem]]
+
+            mp[nums[i]] = i
+
+        return [0,1]  
+
