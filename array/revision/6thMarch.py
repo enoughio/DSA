6fffbaf96ea7e511a36d917aee040807e2ec1f12
aniket <aniketjatav.dev@gmail.com>
2026-03-07 10:41:47 +0530

    optimized numOfPairs solution

--- array/revision/6thMarch.py
+++ array/revision/6thMarch.py
@@ -16,3 +16,36 @@ class Solution:
                     cnt += 1
         
         return cnt
+
+
+        # ---------------------optimized-----------
+
+
+        class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        freq = Counter(nums)
+        
+        ans = 0 
+        for k in nums : 
+            if target.startswith(k): 
+                suffix = target[len(k):]
+                ans += freq[suffix]
+                if k == suffix: ans -= 1
+        return ans 
+    
+
+    # ---=-------more optimal---------
+
+    from collections import Counter
+from typing import List
+
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        freq = Counter(nums)
+        ans = 0 
+        for k, v in freq.items(): 
+            if target.startswith(k): 
+                suffix = target[len(k):]
+                ans += v * freq[suffix]
+                if k == suffix: ans -= freq[suffix]
+        return ans
