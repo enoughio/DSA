9990e2563ce21658a6ac66c3cab3408367195b8c
aniket <aniketjatav.dev@gmail.com>
2026-03-13 10:34:38 +0530

    this question was asked to me in an interview

--- array/revision/13thMarch.py
+++ array/revision/13thMarch.py
@@ -22,3 +22,42 @@ class Solution:
         return ''.join(ans)
 
 
+# ----------------------revers string----------
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        n = len(s)
+        st = 0 
+        ed = n-1
+        ans = ""
+
+        # trim front space 
+        while s[st] == " " :
+            st+=1
+            
+        # trim back space 
+        while s[ed] == " " :
+            ed-=1
+         
+
+        while st <= ed :
+
+            temp = ""
+            while st <= ed and s[st] != " " :
+                temp += s[st]
+                st+=1
+
+            if ans != "" : 
+                ans = temp+ " " + ans
+            else : 
+                ans = temp + ans 
+
+
+            
+            # remove extra space 
+            while st <ed and s[st] == " " :
+                st+=1
+
+        return ans
