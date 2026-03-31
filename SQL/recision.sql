b4766e7c18d4c545012fb83682dc1fa40e59865d
aniket <aniketjatav.dev@gmail.com>
2026-03-31 22:39:08 +0530

    add query to select countries with large area or population

--- SQL/recision.sql
+++ SQL/recision.sql
@@ -17,3 +17,9 @@ ORDER BY product_id ASC
 
 
 -------------
+
+
+# Write your MySQL query statement below
+SELECT name, population, area 
+FROM World 
+WHERE area >= 3000000 OR population >= 25000000 ; 
