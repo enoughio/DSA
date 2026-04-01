cd07958ac6f74757dc0a513dd2e727166f2e1af7
aniket <aniketjatav.dev@gmail.com>
2026-04-01 23:51:09 +0530

    solved sql problem on joins

--- SQL/recision.sql
+++ SQL/recision.sql
@@ -34,3 +34,23 @@ select DISTINCT author_id  as id
 from Views 
 where viewer_id = author_id 
 order by author_id ASC
+
+
+-------------
+
+-- 1683. Invalid Tweets
+select tweet_id 
+from Tweets
+where LENGTH(content) > 15;
+
+
+
+------------\
+--  1581. Customer Who Visited but Did Not Make Any Transactions
+
+select distinct customer_id, count(customer_id) as count_no_trans
+FROM Visits v
+Left Join Transactions t
+on v.visit_id = t.visit_id
+where amount is NULL 
+group by customer_id
