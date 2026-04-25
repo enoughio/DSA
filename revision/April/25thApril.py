d16d06a98e48007819a56eefe92b0cf4d12b06f9
aniket <aniketjatav.dev@gmail.com>
2026-04-25 09:55:16 +0530

    solved an basic question on binary tree

--- revision/April/25thApril.py
+++ revision/April/25thApril.py
@@ -19,4 +19,57 @@ class Solution:
             
         return ans
     
+    # ----------- optimized solution ----------
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        ans = [-1]*n
+        st = []
+
+        for i in range(2*n-1, -1, -1) : 
+            idx = i%n
+
+            while st and st[-1] <= nums[idx] : 
+                st.pop()
+            
+            if i < n and st : 
+                    ans[i] = st[-1]
+
+            st.append(nums[idx])
+                
+                
+        return ans
+
+
+# --------------- Max Depth of Binary tree ------------ 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if root is None : 
+            return 0
+            
+        return self.DFS(root)
+
+    
+    def DFS(self, node) : 
+
+        if node is None : 
+            return 0
+        
+        r = self.DFS(node.left)
+        l = self.DFS(node.right)
+
+        return max(l,r) + 1
+    
     
