5d1b266b134e78eebfe3474db9e0b448fce99a21
aniket <aniketjatav.dev@gmail.com>
2026-05-03 13:52:24 +0530

    solved remove kth digit in a more optimal way

--- revision/May/2ndMay.py
+++ revision/May/2ndMay.py
@@ -35,4 +35,39 @@ class Solution:
         if not ste : 
             return "0"
         return ste
+
+# ----------- more optimized -------------
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        
+        n = len(num)
+        
+        if k >= n : 
+            return "0"
+
+        st = []
+        for i in range(n) : 
+
+            while st and k > 0 and int(st[-1]) > int(num[i]) : 
+                st.pop()
+                k = k-1
+            
+            if not st and num[i] == "0" : 
+                continue
+            
+            st.append(num[i])
+    
+        while st and k > 0 : 
+            st.pop()
+            k = k-1
+        
+        res = "".join(st)
+        if res == "" : 
+            return "0"
+        return res
+
+      
+        
+
         
