e2b0e6e4ba169a2c0ff44d4b3cf7a6b7f44003af
aniket <aniketjatav.dev@gmail.com>
2026-05-18 00:02:15 +0530

    solved a basic question on binary search tree

--- revision/May/17thMay.py
+++ revision/May/17thMay.py
@@ -28,3 +28,43 @@ class Solution:
             parent.right = TreeNode(val)
 
         return root
+
+
+
+# -----------------  fixed 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        self.parent = None
+        def search(node) : 
+
+            if node is None : 
+                return self.parent
+
+            self.parent = node
+            if node.val > val : 
+                search(node.left)
+            else :
+                search(node.right)
+                 
+            return self.parent
+        
+
+        if root is None : 
+            return TreeNode(val)
+
+        parent = search(root)
+        if parent.val > val : 
+            parent.left = TreeNode(val)
+        else : 
+            parent.right = TreeNode(val)
+
+        return root
